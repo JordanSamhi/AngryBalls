ba1c51ea10e73866d6ce5855d52effa86384ca7d
mac <mac@10.10.214.11>
2018-01-31 11:53:26 +0100

    commiit change

--- exodecorateur_angryballs_maladroit_sources/ejbModule/exodecorateur_angryballs/maladroit/AnimationBilles.java
+++ exodecorateur_angryballs_maladroit_sources/ejbModule/exodecorateur_angryballs/maladroit/AnimationBilles.java
@@ -6,10 +6,10 @@ import exodecorateur_angryballs.maladroit.modele.Bille;
 import exodecorateur_angryballs.maladroit.vues.VueBillard;
 
 /**
- * responsable de l'animation des billes, c-à-d responsable du mouvement de la liste des billes. met perpétuellement à jour les billes. 
- * gère le délai entre 2 mises à jour (deltaT) et prévient la vue responsable du dessin des billes qu'il faut mettre à jour la scène
+ * responsable de l'animation des billes, c-ï¿½-d responsable du mouvement de la liste des billes. met perpï¿½tuellement ï¿½ jour les billes. 
+ * gï¿½re le dï¿½lai entre 2 mises ï¿½ jour (deltaT) et prï¿½vient la vue responsable du dessin des billes qu'il faut mettre ï¿½ jour la scï¿½ne
  * 
- * ICI : IL N'Y A RIEN A CHANGER
+ * ICI : IL N'Y A RIEN A CHANGER soumaya
  * */
 public class AnimationBilles  implements Runnable
 {
@@ -17,7 +17,7 @@ public class AnimationBilles  implements Runnable
 
 Vector<Bille> billes;   // la liste de toutes les billes en mouvement 
 VueBillard vueBillard;    // la vue responsable du dessin des billes
-private Thread thread;    // pour lancer et arrêter les billes
+private Thread thread;    // pour lancer et arrï¿½ter les billes
 
 
 private static final double COEFF = 0.5;
@@ -38,48 +38,48 @@ public void run()
 {
 try
     {
-    double deltaT;  // délai entre 2 mises à jour de la liste des billes
+    double deltaT;  // dï¿½lai entre 2 mises ï¿½ jour de la liste des billes
     Bille billeCourante;
     
-    double minRayons = AnimationBilles.minRayons(billes);   //nécessaire au calcul de deltaT
-    double minRayons2 = minRayons*minRayons;                //nécessaire au calcul de deltaT
+    double minRayons = AnimationBilles.minRayons(billes);   //nï¿½cessaire au calcul de deltaT
+    double minRayons2 = minRayons*minRayons;                //nï¿½cessaire au calcul de deltaT
     
     while (!Thread.interrupted())                           // gestion du mouvement
         {
-        //deltaT = COEFF*minRayons2/(1+maxVitessesCarrées(billes));       // mise à jour deltaT. L'addition + 1 est une astuce pour éviter les divisions par zéro
+        //deltaT = COEFF*minRayons2/(1+maxVitessesCarrï¿½es(billes));       // mise ï¿½ jour deltaT. L'addition + 1 est une astuce pour ï¿½viter les divisions par zï¿½ro
         
                                                                         //System.err.println("deltaT = " + deltaT);
         deltaT = 10;
         
         int i;
-        for ( i = 0; i < billes.size(); ++i)    // mise à jour de la liste des billes
+        for ( i = 0; i < billes.size(); ++i)    // mise ï¿½ jour de la liste des billes
             {
             billeCourante = billes.get(i);
-            billeCourante.déplacer(deltaT);                 // mise à jour position et vitesse de cette bille
-            billeCourante.gestionAccélération(billes);      // calcul de l'accélération subie par cette bille
+            billeCourante.dï¿½placer(deltaT);                 // mise ï¿½ jour position et vitesse de cette bille
+            billeCourante.gestionAccï¿½lï¿½ration(billes);      // calcul de l'accï¿½lï¿½ration subie par cette bille
             billeCourante.gestionCollisionBilleBille(billes);
             billeCourante.collisionContour( 0, 0, vueBillard.largeurBillard(), vueBillard.hauteurBillard());        //System.err.println("billes = " + billes);
             }
         
-        vueBillard.miseAJour();                                // on prévient la vue qu'il faut redessiner les billes
+        vueBillard.miseAJour();                                // on prï¿½vient la vue qu'il faut redessiner les billes
       
        
-        Thread.sleep((int)deltaT);                          // deltaT peut être considéré comme le délai entre 2 flashes d'un stroboscope qui éclairerait la scène
+        Thread.sleep((int)deltaT);                          // deltaT peut ï¿½tre considï¿½rï¿½ comme le dï¿½lai entre 2 flashes d'un stroboscope qui ï¿½clairerait la scï¿½ne
         }
     }
 
 catch (InterruptedException e)
     {
-    /* arrêt normal, il n'y a rien à faire dans ce cas */
+    /* arrï¿½t normal, il n'y a rien ï¿½ faire dans ce cas */
     }
 
 }
 
 /**
- * calcule le maximum de de la norme carrée (pour faire moins de calcul) des vecteurs vitesse de la liste de billes
+ * calcule le maximum de de la norme carrï¿½e (pour faire moins de calcul) des vecteurs vitesse de la liste de billes
  * 
  * */
-static double maxVitessesCarrées(Vector<Bille> billes)
+static double maxVitessesCarrï¿½es(Vector<Bille> billes)
 {
 double vitesse2Max = 0;
 
@@ -87,7 +87,7 @@ int i;
 double vitesse2Courante;
 
 for ( i = 0; i < billes.size(); ++i)
-    if ( (vitesse2Courante = billes.get(i).vitesse.normeCarrée()) > vitesse2Max)
+    if ( (vitesse2Courante = billes.get(i).vitesse.normeCarrï¿½e()) > vitesse2Max)
        vitesse2Max = vitesse2Courante; 
 
 return vitesse2Max;
@@ -122,7 +122,7 @@ if (this.thread == null)
     }
 }
 
-public void arrêterAnimation()
+public void arrï¿½terAnimation()
 {
 if (thread != null)
     {
