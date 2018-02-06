86f2cdd16613ea025bb7c4870b4a262a857db384
jordan <jordan@jordanPC.lan>
2018-02-06 19:03:45 +0100

    test

--- exodecorateur_angryballs_maladroit_sources/src/projet/TestAngryBalls.java
+++ exodecorateur_angryballs_maladroit_sources/src/projet/TestAngryBalls.java
@@ -9,8 +9,7 @@ import projet.vues.CadreAngryBalls;
 
 public class TestAngryBalls{
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args){
 		CadreAngryBalls cadre = new CadreAngryBalls(" Angry balls", " Projet Design Pattern SAMHI - ALMEFTAH - AITAAZIZE");
 		cadre.montrer();
 		
