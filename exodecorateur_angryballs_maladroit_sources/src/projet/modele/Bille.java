729b37c7858ea975700c25d952d7c0b22f747126
jordan <jordan@jordanPC.lan>
2018-03-31 18:06:08 +0200

    e

--- exodecorateur_angryballs_maladroit_sources/src/projet/modele/Bille.java
+++ exodecorateur_angryballs_maladroit_sources/src/projet/modele/Bille.java
@@ -8,17 +8,17 @@ import java.util.Vector;
 import mesmaths.geometrie.base.Vecteur;
 
 public abstract class Bille{
-	public abstract void gestionAcc√©l√©ration(Vector<Bille> billes);
+	public abstract void gestionAccÈlÈration(Vector<Bille> billes);
 	public abstract Vecteur getPosition();
 	public abstract double getRayon();
 	public abstract Color getColor();
 	public abstract Vecteur getVitesse();
-	public abstract Vecteur getAcc√©l√©ration();
+	public abstract Vecteur getAccÈlÈration();
 	public abstract int getClef();
 	public abstract double masse();
-	public abstract void d√©placer( double deltaT);
+	public abstract void dÈplacer( double deltaT);
 	public abstract Vecteur gestionCollisionBilleBille(Vector<Bille> billes);
-	public abstract boolean collisionContour(double abscisseCoinHautGauche, double ordonn√©eCoinHautGauche, double largeur, double hauteur);
+	public abstract boolean collisionContour(double abscisseCoinHautGauche, double ordonnÈeCoinHautGauche, double largeur, double hauteur);
 	public abstract void dessine (Graphics g);
 	public abstract void setVitesse(Vecteur vitesse);
 	public abstract void setPosition(Vecteur position);
@@ -28,7 +28,7 @@ public abstract class Bille{
 	
 	public String toString(){
 		return "centre = " + this.getPosition() + " rayon = "+ this.getRayon()+  " vitesse = " + this.getVitesse() 
-		+ " acc√©l√©ration = " + this.getAcc√©l√©ration()+ " couleur = " + this.getColor() + "clef = " + this.getClef();
+		+ " acc√©l√©ration = " + this.getAccÈlÈration()+ " couleur = " + this.getColor() + "clef = " + this.getClef();
 	}
 	
 	public double getDiametre() {
